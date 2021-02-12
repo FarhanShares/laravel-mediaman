0f63c203ea47da6768038e077e3b0a30de595714
Farhan Israq <ishraaq21@gmail.com>
2021-02-12 15:44:13 +0600

    refact: migration publishing

--- src/MediaManServiceProvider.php
+++ src/MediaManServiceProvider.php
@@ -28,42 +28,60 @@ class MediaManServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Migrations
-        if (!class_exists('CreateMediaManCollectionFileTable')) {
-            $this->publishes([
-                __DIR__ . '/../database/migrations/create_mediaman_collection_file_table.stub' => database_path(
-                    'migrations/mediaman/' . date('Y_m_d_His', time()) . '_create_mediaman_collection_file_table.php'
-                ),
-            ], 'migrations');
-        }
+        $migrations = [
+            [
+                'class_name'      => 'CreateMediaManCollectionMediaTable',
+                'file_name'       => 'create_mediaman_collection_media_table'
+            ],
+            [
+                'class_name'      => 'CreateMediaManCollectionsTable',
+                'file_name'       => 'create_mediaman_collections_table'
+            ],
+            [
+                'class_name'      => 'CreateMediaManMediaTable',
+                'file_name'       => 'create_mediaman_media_table'
+            ],
+            [
+                'class_name'      => 'CreateMediamanMediablesTable',
+                'file_name'       => 'create_mediaman_mediables_table'
+            ],
+        ];
 
-        if (!class_exists('CreateMediaManCollectionsTable')) {
-            $this->publishes([
-                __DIR__ . '/../database/migrations/create_mediaman_collections_table.stub' => database_path(
-                    'migrations/mediaman/' . date('Y_m_d_His', time()) . '_create_mediaman_collections_table.php'
-                ),
-            ], 'migrations');
-        }
-
-        if (!class_exists('CreateMediaManFilesTable')) {
-            $this->publishes([
-                __DIR__ . '/../database/migrations/create_mediaman_files_table.stub' => database_path(
-                    'migrations/mediaman/' . date('Y_m_d_His', time()) . '_create_mediaman_files_table.php'
-                ),
-            ], 'migrations');
-        }
-
-        if (!class_exists('CreateMediamanMediablesTable')) {
-            $this->publishes([
-                __DIR__ . '/../database/migrations/create_mediaman_mediables_table.stub' => database_path(
-                    'migrations/mediaman/' . date('Y_m_d_His', time()) . '_create_mediaman_mediables_table.php'
-                ),
-            ], 'migrations');
-        }
+        $this->publishMigrations($migrations);
 
         // Config
         $this->publishes([
             __DIR__ . '/../config/mediaman.php' => config_path('mediaman.php'),
         ], 'config');
+    }
+
+
+    /**
+     * Migration publishing helpers
+     *
+     */
+
+    protected function getMigrationFileSource(string $name, string $ext = '.stub.php')
+    {
+        return __DIR__ . '/../database/migrations/' . $name . $ext;
+    }
+
+    protected function getMigrationFileDestination(string $name, string $ext = '.php')
+    {
+        return database_path(
+            'migrations/mediaman/' . date('Y_m_d_His', time()) . $name . $ext
+        );
+    }
+
+    protected function publishMigrations(array $migrations, string $tag = 'migrations')
+    {
+        foreach ($migrations as $migration) {
+            if (!class_exists($migration['class_name'])) {
+                $this->publishes([
+                    $this->getMigrationFileSource($migration['file_name']) =>
+                    $this->getMigrationFileDestination($migration['file_name'])
+                ], $tag);
+            }
+        }
     }
 }
