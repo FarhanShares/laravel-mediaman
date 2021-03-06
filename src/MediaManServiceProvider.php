635936a7fb37fe8567cb4207167643518cb67feb
Farhan Israq <ishraaq21@gmail.com>
2021-03-06 20:55:58 +0600

    refact: merged migrations into one file

--- src/MediaManServiceProvider.php
+++ src/MediaManServiceProvider.php
@@ -28,61 +28,15 @@ class MediaManServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // the order is important for the migrations to complete
-        $migrations = [
-            [
-                'class_name'      => 'CreateMediaManCollectionsTable',
-                'file_name'       => 'create_mediaman_collections_table'
-            ],
-            [
-                'class_name'      => 'CreateMediaManMediaTable',
-                'file_name'       => 'create_mediaman_media_table'
-            ],
-            [
-                'class_name'      => 'CreateMediaManCollectionMediaTable',
-                'file_name'       => 'create_mediaman_collection_media_table'
-            ],
-            [
-                'class_name'      => 'CreateMediamanMediablesTable',
-                'file_name'       => 'create_mediaman_mediables_table'
-            ],
-        ];
-
-        $this->publishMigrations($migrations);
+        // Migrations
+        $this->publishes([
+            __DIR__ . '/../database/migrations/create_mediaman_tables.php.stub' =>
+            database_path('migrations/' . date('Y_m_d_His', time()) . '_create_mediaman_tables.php')
+        ], 'migrations');
 
         // Config
         $this->publishes([
             __DIR__ . '/../config/mediaman.php' => config_path('mediaman.php'),
         ], 'config');
-    }
-
-
-    /**
-     * Migration publishing helpers
-     *
-     */
-
-    protected function getMigrationFileSource(string $name, string $ext = '.stub')
-    {
-        return __DIR__ . '/../database/migrations/' . $name . $ext;
-    }
-
-    protected function getMigrationFileDestination(string $name, int $index, string $ext = '.php')
-    {
-        return database_path(
-            'migrations/' . date('Y_m_d_His', (time() + $index)) . '_' . $name . $ext
-        );
-    }
-
-    protected function publishMigrations(array $migrations, string $tag = 'migrations')
-    {
-        foreach ($migrations as $index => $migration) {
-            if (!class_exists($migration['class_name'])) {
-                $this->publishes([
-                    $this->getMigrationFileSource($migration['file_name']) =>
-                    $this->getMigrationFileDestination($migration['file_name'], $index)
-                ], $tag);
-            }
-        }
     }
 }
